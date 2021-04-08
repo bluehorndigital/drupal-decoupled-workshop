4d060b11c3f7a4a997819b8bcb11e91c2fb1f1c8
Matt Glaman <nmd.matt@gmail.com>
2021-04-08 10:02:29 -0500

    Fix Lando and DDEV settings loading

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -29,16 +29,17 @@ $settings['cache']['bins']['jsonapi_normalizations']= 'cache.backend.null';
 $settings['hash_salt'] = '384SXF14RJjfKzgXfwXW-2ls5gbpKtgNIzMWQAS3tpSScMs-ThV4kJoCfCATQDcIT7X8r0H_RA';
 
 // Include settings for Lando.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') == 'true') {
-  require $ddev_settings;
+$lando_settings = __DIR__ . '/settings.lando.php';
+if (is_readable($lando_settings) && getenv('LANDO') === 'ON') {
+  require $lando_settings;
 }
 
 // Automatically generated include for settings managed by ddev.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') == 'true') {
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') === 'true') {
   require $ddev_settings;
 }
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
